06975a8c26cf12ca80a1f518d12bc6a8e8231e11
jamilshehab <jamilshehab>
2026-06-12 19:30:53 +0300

    fixing error on production

--- frontend/sanity/lib/client.ts
+++ frontend/sanity/lib/client.ts
@@ -3,8 +3,6 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  apiVersion: "2026-06-04",
   useCdn: true, // important for speed (cache)
 });
-
-
